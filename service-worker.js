7ee378bf3664b486037b0e44b22528ddc8208f9a
AJ <amcquay@gmail.com>
2026-07-11 18:03:09 -0600

    fix clipping in "today's session"

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
  * Bump CACHE_VERSION whenever the precache list or strategy changes; old caches are pruned.
  */
 
-const CACHE_VERSION = "v20";
+const CACHE_VERSION = "v23";
 const SHELL_CACHE   = `fretboard-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `fretboard-runtime-${CACHE_VERSION}`;
 
